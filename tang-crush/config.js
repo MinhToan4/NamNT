619c3e8dc71c6b77aa02e6ab9918013878298eeb
MinhToan4 <87869289+MinhToan4@users.noreply.github.com>
2023-10-14 01:06:24 +0700

    Add files via upload

---
+++ tang-crush/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Nam Ngố!',
+    introDesc: `Đại gia khu xóm chợ.`,
+    btnIntro: 'Chốt!',
+    title: 'Nhìn phát biết dân chơi!',
+    desc: 'Phải chăng say nắng anh ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Anh là số 1!',
+    btnNo: 'Nhảm xịt 😒',
+    question:'Chú được cái nói đúng!',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Anh đỉnh, anh 10 điểm. 💯',
+    mess: 'Anh biết mà 🥰.',
+    messDesc: 'Giữ gìn sức khỏe nhá ông chú!',
+    btnAccept: 'Okiiiii lun 😘',
+    messLink: 'https://www.facebook.com/profile.php?id=100045173529851' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
